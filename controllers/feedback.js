37eea4ebc8d78e946969926a36236e4dcca8c050
javed <mohd.javed@trigma.in>
2020-02-11 18:13:52 +0530

    working on feedbacks

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -18,18 +18,60 @@ controller.post('/getFeedBacks', async (req, res) => {
     // Fetch UserDetails using Auth Token
     let skip = (parseInt(req.body.pageNumber) * parseInt(req.body.pageSize)) - parseInt(req.body.pageSize);
     let limit = parseInt(req.body.pageSize);
-       feedBackSchema.getFeedBackList({skip: skip, limit: limit }, async function (err, resp) {
-                    if (err) { return res.status(def.API_STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR).send({ response: err }); }
-                    
-                        
+    feedBackSchema.getFeedBackList({ skip: skip, limit: limit }, async function (err, resp) {
+        if (err) { return res.status(def.API_STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR).send({ response: err }); }
 
-                       res.status(def.API_STATUS.SUCCESS.OK).send({ totalItems: resp.count[0].totalItem, feedback: resp.feedback, });
-       })
+
+
+        res.status(def.API_STATUS.SUCCESS.OK).send({ totalItems: resp.count[0].totalItem, feedback: resp.feedback, });
+    })
 
 });
 
 
 
+controller.post('/getFeedBackDetails', async (req, res) => {
+    feedBackSchema.getFeedBackDetails({ userId: req.body.userId, projectId: req.body.projectId }, async function (err, resp) {
+        if (err) { return res.status(def.API_STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR).send({ response: err }); }
+
+        res.status(def.API_STATUS.SUCCESS.OK).send({ feedback: resp.feedback, });
+
+    })
+
+});
+
+
+controller.post('/getCompletedProjects', async (req, res) => {
+    var authToken = req.headers['x-auth-token'];
+
+    //Verify User 
+    userSchema.fetchUserByAuthToken(authToken, async function (err, userDetails) {
+        feedBackSchema.getCompletedProjects({ userId: userDetails[0].user_id }, async function (err, resp) {
+            if (err) { return res.status(def.API_STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR).send({ response: err }); }
+
+            res.status(def.API_STATUS.SUCCESS.OK).send({ projects: resp.projects, });
+
+        })
+    });
+
+});
+
+
+
+controller.post('/saveFeedBack', async (req, res) => {
+    var authToken = req.headers['x-auth-token'];
+
+    //Verify User 
+    userSchema.fetchUserByAuthToken(authToken, async function (err, userDetails) {
+        feedBackSchema.saveFeedBack({ userId: userDetails[0].user_id ,body:req.body}, async function (err, resp) {
+            if (err) { return res.status(def.API_STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR).send({ response: err }); }
+
+            res.status(def.API_STATUS.SUCCESS.OK).send(true);
+
+        })
+    });
+
+});
 
 
 module.exports = controller;
